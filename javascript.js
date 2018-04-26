4c27ca3988cefd709c0fc2c7c89f0ad6810afe13
Ishan Subedi <ikushum@gmail.com>
2018-04-27 00:22:40 +0545

    play inition video using Youtube IFrame Player API

--- javascript.js
+++ javascript.js
@@ -1,6 +1,35 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Nepal!'
-  }
+    message: 'Hello Nepal!',
+    player: null,
+    done: false
+  },
+	methods: {
+		onPlayerReady(event) {
+		    event.target.playVideo();
+		},
+		onPlayerStateChange(event) {
+			if (event.data == YT.PlayerState.PLAYING && !done) {
+			  setTimeout(this.stopVideo, 6000);
+			  this.done = true;
+			}
+		},
+		stopVideo() {
+			this.player.stopVideo();
+		}					
+	},
+  created() {
+	window.onYouTubeIframeAPIReady = () => {
+		this.player = new YT.Player('player', {
+			height: '390',
+			width: '640',
+			videoId: 'M7lc1UVf-VE',
+			events: {
+			'onReady': this.onPlayerReady,
+			'onStateChange': this.onPlayerStateChange
+			}
+		})
+    }
+  },
 })
