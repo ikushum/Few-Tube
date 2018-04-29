3ed9d793f4a01a1045d51f825396cfb624b3a690
Ishan Subedi <ikushum@gmail.com>
2018-04-29 21:22:55 +0545

    implement search

--- javascript.js
+++ javascript.js
@@ -3,9 +3,42 @@ var app = new Vue({
   data: {
     message: 'Hello Nepal!',
     player: null,
-    done: false
+    done: false,
+    showPlayer: false,
+    searchKeyword: ''
   },
 	methods: {
+		enablePlayer () {
+			this.showPlayer = true
+			this.player = new YT.Player('player', {
+				height: '390',
+				width: '640',
+				videoId: 'M7lc1UVf-VE',
+				events: {
+					'onReady': this.onPlayerReady,
+					'onStateChange': this.onPlayerStateChange
+				}
+			})
+		    console.log(this.player)
+		},
+		initGoogleApi () {
+		  gapi.client.init({
+		    'apiKey': 'YOUR_API_KEY',
+		    // clientId and scope are optional if auth is not required.
+		    'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+		    'scope': 'profile',
+		  }).then(function() {
+		    // 3. Initialize and make the API request.
+		    return gapi.client.request({
+		      'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+		    })
+		  }).then(function(response) {
+		    console.log(response.result);
+		  }, function(reason) {
+		    console.log('Error: ' + reason.result.error.message);
+		  });
+
+		},
 		onPlayerReady(event) {
 		    event.target.playVideo()
 		},
@@ -14,26 +47,31 @@ var app = new Vue({
 				this.stopVideo()	
 				this.done = true			
 			}
-			if (event.data == YT.PlayerState.PLAYING && !this.done) {
-			  setTimeout(this.stopVideo, 6000)
-			  this.done = true
-			}
 		},
 		stopVideo() {
 			this.player.stopVideo()
+		},
+		search() {			
+			let params = {
+				'maxResults': '25',
+				'part': 'snippet',
+				'q': this.searchKeyword,
+				'type': '',
+				'key': 'AIzaSyARPz0pMc-wmIlVwCtFF26pIOBRX_5oYik'	
+             }
+			axios({
+			  method: 'get',
+			  url: 'https://www.googleapis.com/youtube/v3/search',
+			  params: params,
+			  headers: {'X-ApiKey': 'AIzaSyARPz0pMc-wmIlVwCtFF26pIOBRX_5oYik'}
+			})			
+			  .then(function (response) {
+			    ;
+			  })
+			  .catch(function (error) {
+			    console.log(error);
+			  });
 		}					
-	},
-  created() {
-	window.onYouTubeIframeAPIReady = () => {
-		this.player = new YT.Player('player', {
-			height: '390',
-			width: '640',
-			videoId: 'M7lc1UVf-VE',
-			events: {
-			'onReady': this.onPlayerReady,
-			'onStateChange': this.onPlayerStateChange
-			}
-		})
-    }
-  },
+	}
 })
+
